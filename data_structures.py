5dc24b74bf1789b4b468c415dfca3cdb58ce16d6
mussym <mussym@localhost>
2019-07-05 13:45:06 -0500

    more stuff

--- data_structures.py
+++ data_structures.py
@@ -2,6 +2,7 @@
 
 #import for a deep copy
 import copy
+from math import pi
 
 #import for a queue structure
 from collections import deque
@@ -108,5 +109,62 @@ print(dcopy)
 #using lists as stacks etc
 print("use the pop() function on list to emulate a stack, returns last item added")
 print("do not use a list to emulate a queue because 'popping' first item is slow")
-print("use collections.deque instead")
+print("use collections.deque instead, fast appends and pops from both ends")
 queue = deque(["winter", "summer", "spring", "autumn"])
+queue.append("cold")
+queue.append("warm")
+queue.append("hot")
+queue.append("wet")
+print("queue starts as-: ", ", ".join(queue))
+print("popping from left-: ", queue.popleft())
+print("queue is now-: ", ", ".join(queue))
+print("popping from right -: ", queue.pop())
+print("queue is now-: ", ",".join(queue))
+
+#list comprehensions
+print("-------------------------------")
+print("List Comprehensions")
+print("non side effect method of creating a list in a loop")
+print("Note:one way of doing this is using the factory method list, with parameter 1 being a function or lambda, parameter 2 an iterable")
+print("eg: some_thing = list(map(lambda blah: blah, some_iterable))")
+squares = list(map(lambda x: x**2, range(10)))
+print("note: lambda used with map to convert list of ints to strings")
+print("list of squares-: ", ", ".join(map(lambda x: str(x), squares)))
+print("list comprehension method is more concise and readable and...")
+print("brackets '[ ]', expression, for clause, then zero or more for or if clauses")
+print("squares = [x**2 for x in range(10)]")
+squares = [x**2 for x in range(10)]
+print(squares)
+print("combining two lists of items that don't match")
+list1 = ["queen", "police", "ramones", "pistols"]
+list2 = ["queen", "police", "ramones", "clash"]
+newlist = [(x, y) for x in list1 for y in list2 if x != y]
+print(newlist)
+vec = [-4, -2, 0, 2, 4]
+doublevec = [x *2 for x in vec]
+print(doublevec)
+non_neg_vec = [x for x in doublevec if x >= 0]
+print(non_neg_vec)
+print("apply function to all items")
+func_vec = [abs(x) for x in doublevec]
+print(func_vec)
+print("creating list of tuples")
+suites = ["spades", "clubs", "diamonds", "hearts"]
+deck = [(suite, digit) for suite in suites for digit in range(1, 11)]
+print(deck)
+print("or  a list of tuples from a calc")
+fun_list = [(x, x * 100) for x in range(5)]
+print(fun_list)
+print("list comprehensions can contain complex expressions and nested functions")
+fun_list = [str(round(pi, i)) for i in range(1, 6)]
+print(fun_list)
+print("the expression in a comprehension can be any expression, including another list comprehension")
+print("however beware of the complexity, use a built in function instead, such as zip()")
+
+print("----------- the del statement -------------")
+print("delete item by index, by slice, or entire list")
+temp_list = ['a', 'b', 'c']
+del temp_list[1:]
+print (temp_list)
+del temp_list
+
